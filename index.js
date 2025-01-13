b9d4fd3340e80550e8c4aaa5220e92bc5e6e3696
OlisehOkiah <okiaholiseh123@gmail.com>
2025-01-13 12:35:19 +0100

    done

--- index.js
+++ index.js
@@ -14,3 +14,22 @@ document.getElementById("scrollButton").addEventListener("click", function(){
     const target = document.getElementById("footers");
     target.scrollIntoView({behavior: "smooth"});
 })
+
+
+$(document).ready(function() {
+    $(`.skill-per`).each(function() {
+      var $this = $(this);
+      var percentage = $this.attr('percentage');
+      $this.css("width", percentage + "%");
+      $({
+        animatedValue: 0
+      }).animate({
+        animatedValue: percentage
+      }, {
+        duration: 1300,
+        step: function() {
+          $this.attr("percentage", Math.floor(this.animatedValue));
+        }
+      });
+    });
+  });
